c8680f62c33f1fbaaa0eb40d84a4c86a5e954f50
jmike <jmike@DESKTOP-54L68HD>
2017-06-27 20:28:23 -0700

    Updated result.Exists to result.Contains() for better readablity

--- nameRando/Program.cs
+++ nameRando/Program.cs
@@ -25,23 +25,25 @@ namespace nameRando
                 string[] names = new string[13] { "Jeff", "Angela", "Chris R.", "Ethan", "Mike", "Justin", "Jessica", "Amanda", "Chris L.", "Brian", "Chris M.", "Juan", "Richard" };
 
                     Random rand = new Random();
-                    List<Int32> result = new List<Int32>();
+                    List<int> result = new List<int>();
 
-                    for (Int32 i = 0; i < 13; i++)
+                    for (int i = 0; i < 13; i++)
                     {
-                        Int32 curName = rand.Next(0, 13);
+                        int curName = rand.Next(0, 13);
 
-                        while (result.Exists(value => value == curName))
+                        while (result.Contains(curName))
                         {
                             curName = rand.Next(0, 13);
                         }
 
-                            result.Add(curName);
-                            string nameNum = names[curName];
-                            Console.WriteLine(nameNum);
-                            Console.WriteLine("type stop to start over or press enter for a new name: ");                           
-                            string newEntry = Console.ReadLine().ToLower();
-                            Console.Clear();
+                        result.Add(curName);
+
+                        string nameNum = names[curName];
+                        Console.WriteLine(nameNum);
+                        Console.WriteLine("type \"stop\" to start over or press enter for a new name: ");                           
+                        string newEntry = Console.ReadLine().ToLower();
+                        Console.Clear();
+
                         if (newEntry == "stop")
                         {
                             break;
